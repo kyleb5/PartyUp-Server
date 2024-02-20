2c5d7d76de10758f2d18130a5dc91deaee8d06c9
Kyle B <kberry1298@gmail.com>
2024-02-19 21:25:08 -0600

    WIP

---
+++ partyupapi/models/lfg_post.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .games import Games
+from .user import User
+
+
+class LFGPost(models.Model):
+    game = models.ForeignKey(Games, on_delete=models.CASCADE)
+    title = models.TextField()
+    description = models.TextField()
+    needed_players = models.IntegerField()
+    skill_level = models.CharField(max_length=20)
+    platform = models.TextField()
+    region = models.TextField()
+    mic_needed = models.BooleanField()
+    status = models.BooleanField()
+    uuid = models.ForeignKey(User, on_delete=models.CASCADE)
