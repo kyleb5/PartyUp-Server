a1efa91bbecdd8b7c3008209c2579fb44b3e2a7a
Kyle B <kberry1298@gmail.com>
2024-02-26 20:47:42 -0600

    Changed timestamp to track time better

--- partyupapi/admin.py
+++ partyupapi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Games, User
+from .models import Games, User, LFGPost
 
 admin.site.register(Games)
 admin.site.register(User)
+admin.site.register(LFGPost)
